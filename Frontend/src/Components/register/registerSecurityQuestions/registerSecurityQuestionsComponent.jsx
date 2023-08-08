b3acef09bf8bc56c97b29b7f64a0956878ebe6d3
Amanjot Singh <am854663@dal.ca>
2023-08-08 19:56:16 -0300

    Chnaged redirects after registering user

--- Frontend/src/Components/register/registerSecurityQuestions/registerSecurityQuestionsComponent.jsx
+++ Frontend/src/Components/register/registerSecurityQuestions/registerSecurityQuestionsComponent.jsx
@@ -43,8 +43,8 @@ function RegisterSecurityQuestion() {
           })
           .then((res) => {
             handleSnackbarOpen("Success Please Confirm your Email!");
-            localStorage.setItem("email", email);
-            setTimeout(() => navigate("/"), 2000);
+
+            setTimeout(() => navigate("/login"), 2000);
           });
       })
       .catch((err) => {
